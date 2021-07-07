9f189a7ee487fe2394c85de7a061dc510ff58545
Jamie McClelland <jm@mayfirst.org>
2021-07-07 15:13:09 -0400

    let the petitionemail extension hide it's own fields.
    See: https://github.com/josephlacey/com.aghstrategies.petitionemail/pull/1

--- js/petitionemailgroup.js
+++ js/petitionemailgroup.js
@@ -1,52 +1,32 @@
 (function($){
-  //Initial recipient system processing
+  // Locate the field controlling the recipient email system.
   CRM.api3('CustomField', 'getsingle', {
     "return": ["id"],
     "name": "Email_Recipient_System"
   }).done(function(recipientSystemField) {
-    //Page load
-    recipient_system_group(recipientSystemField.id);
-    //When the salutation type changes
+    // Display the right system based on the initial value.
+    update_group_recipient_display(recipientSystemField.id);
+    //When the recipient system is changed, update the display. 
     CRM.$("select[id*='custom_" + recipientSystemField.id + "']").change(function() {
-      recipient_system_group(recipientSystemField.id);
+      update_group_recipient_display(recipientSystemField.id);
     });
   });
 
 })(CRM.$);
 
-function recipient_system_group (fieldId){
+function update_group_recipient_display(fieldId){
   //Process the selected option
   CRM.api3('CustomField', 'getsingle', {
     "return": ["id"],
     "name": 'Recipient_Group'
   }).done(function(recipientGroupField) {
-    if (CRM.$("select[id*='custom_" + fieldId + "'] option:selected").val() == 'Group') {
-      //Show Group Field field
+    var selectedSystem = CRM.$("select[id*='custom_" + fieldId + "'] option:selected").val();
+    if ( selectedSystem == 'Group') {
+      // Show Group Field
       CRM.$("tr[class*='custom_" + recipientGroupField.id + "']").show();
-      recipient_single_hide_group(true);
     }
-    if (CRM.$("select[id*='custom_" + fieldId + "'] option:selected").val() != 'Group') {
+    else {
       CRM.$("tr[class*='custom_" + recipientGroupField.id + "']").hide();
-    }
-    if (CRM.$("select[id*='custom_" + fieldId + "'] option:selected").val() == 'Single') {
-      recipient_single_hide_group(false);
-    }
-  });
-}
-
-function recipient_single_hide_group(hide){
-  CRM.api3('CustomField', 'get', {
-    "return": ["id"],
-    "name": {"IN":["Recipient_Name","Recipient_Email"]}
-  }).done(function(recipientSingle) {
-    if (hide) {
-      CRM.$.each(recipientSingle.values, function(id,fieldId) {
-        CRM.$("tr[class*='custom_" + id + "']").hide();
-      });
-    } else {
-      CRM.$.each(recipientSingle.values, function(id, fieldId) {
-        CRM.$("tr[class*='custom_" + id + "']").show();
-      });
     }
   });
 }
